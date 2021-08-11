1fe69339c332ba9ac5c322c34598dd31d7b63434
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2021-08-11 15:59:46 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-# 
-# Create Bash aliases to the helper scripts.
 
-PROJECT_ROOT=$(dirname "$BASH_SOURCE")
-
-alias til="$PROJECT_ROOT/scripts/til.sh"
+alias til="~/dev/til/scripts/til.sh"
